4b76a155fd8e7d94efcd8a9cc28c85ee369bce87
Alex Ivanov <aivanov@microsoft.com>
2021-01-04 11:17:26 -0500

    my supper web site

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AZ400.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
